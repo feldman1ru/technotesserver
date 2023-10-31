f2298e34e7efe01e9579f33602e41042afce5db6
feldman1ru <92920736+feldman1ru@users.noreply.github.com>
2023-10-31 20:29:20 +0200

    com4

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,13 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: '/*',
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     credentials: true,
     optionsSuccessStatus: 200
 }
